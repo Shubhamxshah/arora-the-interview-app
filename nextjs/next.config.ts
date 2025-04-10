061ebd969be3b5d98159ef570a276d1f7cd7dab4
Shubhamxshah <ritikgujarat@gmail.com>
2025-04-10 19:39:44 +0530

    added next config

--- nextjs/next.config.ts
+++ nextjs/next.config.ts
@@ -3,12 +3,16 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    domains: [
-      'd39st1yc0t52gi.cloudfront.net'
-    ]
-  }, 
-  serverExternalPackages : ['pdf2json'],
-  allowedDevOrigins: ['http://213.136.68.185:3000', 'http://arora.shubhamxshah.xyz', 'https://arora.shubhamxshah.xyz']
+    domains: ["d39st1yc0t52gi.cloudfront.net"],
+  },
+  serverExternalPackages: ["pdf2json"],
+  experimental: {
+    allowedDevOrigins: [
+      "http://213.136.68.185:3000",
+      "http://arora.shubhamxshah.xyz",
+      "https://arora.shubhamxshah.xyz",
+    ],
+  },
 };
 
 export default nextConfig;
